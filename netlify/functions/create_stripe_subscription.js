5c6f60faf60560d1b48b68b75e0144b18bc3eb57
JacksonBey <jacksongrowson@gmail.com>
2023-04-04 22:17:48 -0700

    added error handling and updated api route

--- netlify/functions/create_stripe_subscription.js
+++ netlify/functions/create_stripe_subscription.js
@@ -17,16 +17,28 @@ const bigCommerce = new BigCommerce({
 // note: subscriptionLineItem, deliveryDateOption, billing_cycle_anchor finding subject to change depending on data structure
 exports.handler = async (event) => {
   try {
+    if (!event.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Invalid request payload' }),
+      };
+    }
     const payload = JSON.parse(event.body);
+
+    if (!payload || !payload.data || !payload.data.id) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Invalid request payload' }),
+      };
+    }
+
     const orderId = payload.data.id;
+    console.log('orderId', orderId)
 
-    // const order = await bigCommerce.get(`/orders/${orderId}`);
-    const orderProducts = await bigCommerce.get(`/orders/${orderId}/products?include=lineItems.options`);
-    
-    const consignments = await bigCommerce.get(`/orders/${orderId}/consignments`);
+    const order = await bigCommerce.get(`/orders/${orderId}`);
 
-    // console.log('OPTIONS', orderProducts[0].product_options);
-
+    const orderProducts = await bigCommerce.get(`/orders/${orderId}/products?include=options`);
+    console.log('orderProductsResponse', orderProducts)
 
     // Find the subscription line item and related data
     const subscriptionLineItem = orderProducts.find(
@@ -37,10 +49,15 @@ exports.handler = async (event) => {
       // deliveryDateOption = subscriptionLineItem.product_options.find(
       //   (option) => option.name === 'Delivery Date'
       // );
-      const customFieldsResponse = await bigCommerce.get(
-        `/catalog/products/${subscriptionLineItem.id}/custom-fields`
-      );
-
+      console.log('subscriptionLineItem', subscriptionLineItem.product_id)
+      let customFieldsResponse;
+      const customFieldsUrl = `https://${process.env.STORE_HASH}.mybigcommerce.com/api/v2/products/${subscriptionLineItem.product_id}/custom_fields.json`;
+      try {
+        customFieldsResponse = await bigCommerce.get(customFieldsUrl);
+      } catch (error) {
+        console.error(`Error fetching custom fields for product ${subscriptionLineItem.product_id}:`, error);
+        customFieldsResponse = { data: [] }; // Set an empty custom fields array if an error occurs
+      }
       console.log('customFieldsResponse', customFieldsResponse)
       
       const customFields = customFieldsResponse.data;
@@ -48,13 +65,21 @@ exports.handler = async (event) => {
       const productOptions = subscriptionLineItem.product_options;
       console.log('productOptions', productOptions)
       // productOptions [ { id: 101, name: 'Delivery Date', value: '2023-04-10' } ] 
-      const deliveryDateOption = productOptions.find(option => option.name === 'Delivery Date');
+      const deliveryDateOption = productOptions.find(option => option.display_name == 'Pick-up Date');
+      console.log('deliveryDateOption', deliveryDateOption)
    
       const stripeProductIdField = customFields.find(field => field.name === 'Stripe Product ID');
       const stripePriceIdField = customFields.find(field => field.name === 'Stripe Price ID');
       
-      const stripeProductId = stripeProductIdField ? stripeProductIdField.value : null;
-      const stripePriceId = stripePriceIdField ? stripePriceIdField.value : null;
+      // const stripeProductId = stripeProductIdField ? stripeProductIdField.value : null;
+      // const stripePriceId = stripePriceIdField ? stripePriceIdField.value : null;
+
+      let stripeCustomer = await stripe.customers.list({ email: order.email });
+      const stripeCustomerId = stripeCustomer.data[0].id;
+      const paymentMethods = await stripe.paymentMethods.list({
+        customer: stripeCustomerId,
+        type: 'card',
+      });
       
 
       if (deliveryDateOption) {
@@ -63,77 +88,70 @@ exports.handler = async (event) => {
       } else {
         console.log('Delivery date option not found');
       }
+
+      try {
+        const customers = await stripe.customers.list({ email: order.email });
+        stripeCustomer = customers.data[0];
+      } catch (error) {
+        console.error('Error fetching customer:', error);
+      }
+
+      if (!stripeCustomer) {
+        stripeCustomer = await stripe.customers.create({
+          email: order.email,
+          name: `${order.billing_address.first_name} ${order.billing_address.last_name}`,
+          /* map the order.billing_address fields to Stripe address fields */ 
+          address: { 
+            line1: order.billing_address.street_1,
+            line2: order.billing_address.street_2,
+            city: order.billing_address.city,
+            state: order.billing_address.state,
+            postal_code: order.billing_address.zip,
+            country: order.billing_address.country_iso2,
+          },
+        });
+      }
+
+
+      // Parse the date modifier value into a JavaScript Date object
+      // const deliveryDate = new Date(deliveryDateOption.value);
+      const deliveryDate = new Date(deliveryDateOption.value);
+      console.log('PAYMENT METHODS: ', paymentMethods)
+      const defaultPaymentMethod = paymentMethods?.data[0]?.id;
+      const subscriptionCreateParams = {
+        customer: stripeCustomer.id,
+        default_payment_method: defaultPaymentMethod,
+        items: [{ price: "price_1Mst3NGOO2ZYN95StCmNH1ub" }],
+        ...(isNaN(deliveryDate) ? {} : { billing_cycle_anchor: deliveryDate })
+      };
+
+      // Calculate the timestamp (in seconds) for the billing_cycle_anchor
+      const billingCycleAnchorTimestamp = Math.floor(deliveryDate.getTime() / 1000);
+
+      console.log('subscription create: ', subscriptionCreateParams)
+
+      console.log('subscriptiopnline item: ', subscriptionLineItem)
+
+      // const subscription = await stripe.subscriptions.create({
+      //   customer: stripeCustomer.id,
+      //   items: [{ price: "price_1Mst3NGOO2ZYN95StCmNH1ub"}],
+      //   /* calculate the timestamp based on the date modifier value */
+      //   billing_cycle_anchor:  billingCycleAnchorTimestamp,
+      // });
+      const subscription = await stripe.subscriptions.create(subscriptionCreateParams);
+
+      await bigCommerce.post(`/orders/${orderId}/order_messages`, {
+        message: `Stripe subscription ID: ${subscription.id}`,
+        staff_only: true,
+      });
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Subscription created successfully' }),
+      };
     } else {
       console.log('Subscription line item not found');
     }
-
-
-
-    let stripeCustomer;
-
-    try {
-      const customers = await stripe.customers.list({ email: order.email });
-      stripeCustomer = customers.data[0];
-    } catch (error) {
-      console.error('Error fetching customer:', error);
-    }
-
-    if (!stripeCustomer) {
-      stripeCustomer = await stripe.customers.create({
-        email: order.email,
-        name: `${order.billing_address.first_name} ${order.billing_address.last_name}`,
-        /* map the order.billing_address fields to Stripe address fields */ 
-        address: { 
-          line1: order.billing_address.street_1,
-          line2: order.billing_address.street_2,
-          city: order.billing_address.city,
-          state: order.billing_address.state,
-          postal_code: order.billing_address.zip,
-          country: order.billing_address.country_iso2,
-        },
-      });
-    }
-
-
-    // Parse the date modifier value into a JavaScript Date object
-    const deliveryDate = new Date(deliveryDateOption.value);
-
-    // Calculate the timestamp (in seconds) for the billing_cycle_anchor
-    const billingCycleAnchorTimestamp = Math.floor(deliveryDate.getTime() / 1000);
-
-    console.log('subscription create: ', {
-      customer: stripeCustomer.id,
-      items: [{ price: stripePriceId }],
-      /* calculate the timestamp based on the date modifier value */
-      billing_cycle_anchor:  billingCycleAnchorTimestamp,
-    })
-
-    console.log('subscriptiopnline item: ', subscriptionLineItem)
-
-    // const price = await stripe.prices.create({
-    //   currency: 'usd',
-    //   custom_unit_amount: {enabled: true},
-    //   product: subscriptionLineItem.id ,
-    // });
-
-    // console.log('price: ', price)
-
-    const subscription = await stripe.subscriptions.create({
-      customer: stripeCustomer.id,
-      items: [{ price: stripePriceId}],
-      /* calculate the timestamp based on the date modifier value */
-      billing_cycle_anchor:  billingCycleAnchorTimestamp,
-    });
-
-    await bigCommerce.post(`/orders/${orderId}/messages`, {
-      message: `Stripe subscription ID: ${subscription.id}`,
-      staff_only: true,
-    });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Subscription created successfully' }),
-    };
   } catch (error) {
     console.error('Error processing subscription:', error);
 
@@ -143,57 +161,3 @@ exports.handler = async (event) => {
     };
   }
 };
-
-
-
-const sampleOrder = {
-  id: 12345,
-  billing_address: {/* ... */},
-  email: 'john@example.com',
-  products: [
-    {
-      id: 1,
-      name: 'Sample Subscription Product',
-      sku: 'sample-subscription-product',
-      price: 9.99,
-      product_options: [
-        {
-          id: 101,
-          name: 'Delivery Date',
-          value: '2023-04-10',
-        },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Sample Non-Subscription Product',
-      sku: 'sample-non-subscription-product',
-      price: 19.99,
-      product_options: [],
-    },
-  ],
-};
-
-const sampleBillingAddress = {
-  first_name: 'John',
-  last_name: 'Doe',
-  company: 'Acme Inc.',
-  street_1: '123 Main St',
-  street_2: 'Apt 4B',
-  city: 'New York',
-  state: 'NY',
-  zip: '10001',
-  country: 'United States',
-  country_iso2: 'US',
-  phone: '555-123-4567',
-  email: 'john.doe@example.com',
-};
-
-
-const getOrderCUrl = { curl: `
-curl -X GET \
-  -H "Content-Type: application/json" \
-  -H "X-Auth-Token: b6lh1ab3dz7eegnqyj0ityjs0blj5jf" \
-  -H "X-Auth-Client: 84ht9px9puq9uy2yx40xpztwjy47ndq" \
-  "https://api.bigcommerce.com/stores/q2dar3yy52/v3/orders/100?include=products
-`}
